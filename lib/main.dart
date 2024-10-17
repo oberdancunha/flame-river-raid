c4efa78ffca476f7242439a1ee14b8582bde933f
Oberdan Cunha <oberdancunha@gmail.com>
2024-10-17 15:33:31 -0300

    Create splash page for iOS, Android and Android version 12+.

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,19 @@
 import 'package:flame/flame.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 import 'app_widget.dart';
 import 'game/constants/assets.dart';
 import 'game/soloud.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   await Flame.device.setPortrait();
   await Flame.device.fullScreen();
   await Assets.load();
   await initSoLoud();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  await Future.delayed(Duration(seconds: 1), FlutterNativeSplash.remove);
 
   runApp(const AppWidget());
 }
